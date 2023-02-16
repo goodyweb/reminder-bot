acf65bdb967910920adbf442328f7ccd21fe54e1
reymarkjan01 <s.gandalla.reymarkjan@cmu.edu.ph>
2023-02-16 11:33:56 +0800

    Hahaha

--- mark.php
+++ mark.php
@@ -3,4 +3,4 @@ echo "Hello World! in Goody Web Solutions Inc. In CMU relations";
 ?>
 <h1>Reymark Jan T. Gandalla<h1>
 <h2>BS in Information Technology"<h2>
-    
+<h3>Major in Software Development<h3>    
