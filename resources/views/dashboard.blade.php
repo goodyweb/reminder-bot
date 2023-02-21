3a778eeb563c0c9a69920dfa4c69ef7abea2fe5e
reymarkjan01 <s.gandalla.reymarkjan@cmu.edu.ph>
2023-02-21 16:01:24 +0800

    new

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,6 @@
         <div class="row mt-5">
             <div class="col-xl-8 mb-5 mb-xl-0">
                 <div class="card shadow">
-                    
                         <div class="bg-light card-header border-0">
                             <div class="row align-items-center">
                                 <div class="col">
@@ -22,45 +21,38 @@
                             <table class="table table-sm table-hover mb-0 align-items-center table-flush">
                                 <thead class="thead-light">
                                     <tr>
-                                    <th class="pt-0">#</th>
-                                    <th class="pt-0">Reminder Title</th>
-                                    <th class="pt-0">Content Detail</th>
-                                    <th class="pt-0">Description</th>
+                                    <th class="pt-3"></th>
+                                    <th class="pt-3">#</th>
+                                    <th class="pt-3">Reminder Title</th>
+                                    <th class="pt-3">Content Detail</th>
+                                    <th class="pt-3">Description</th>
                                     <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
+
                                 @foreach($reminders as $index => $val)
-                        <tr>
-                            <td>{{++$index}}</td>
-                            <td>{{$val->title}}</td>
-                            <td>{{$val->content}}</td>
-                            <td>{{$val->description}}</td>
-                            
-                            <td> <a href="#" class="btn btn-outline-primary btn-sm">
-                                      View Details
-                                  </a> </td>
-
-
-
-
-                                        </tr>
-                                        @endforeach
+                                <tr>
+                                    <td></td>
+                                    <td>{{++$index}}</td>
+                                    <td>{{$val->title}}</td>
+                                    <td>{{$val->content}}</td>
+                                    <td>{{$val->description}}</td>
+                                    <td> <a href="#" class="btn btn-outline-primary btn-sm">View Details </a></td>
+                                </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
-                    
                         <div class="row mt-3 mb-5">
                             <div class="col text-center">
-                                <a href="" class="btn btn-primary btn-lg">Create Reminders</a>
+                                <a href="{{route('reminders.create')}}" class="btn btn-info btn-icon-text mb-2 mb-md-0">Create Reminders</a>
                             </div>
                         </div>
-                 
                 </div>
             </div>
             <div class="col-xl-4 mb-5 mb-xl-0">
                 <div class="card shadow">
-                    
                         <div class="bg-light card-header border-0">
                             <div class="row align-items-center">
                                 <div class="col">
@@ -75,23 +67,19 @@
                             <table class="table table-sm table-hover mb-0 align-items-center table-flush">
                                 <thead class="thead-light">
                                     <tr>
-                                    <th scope="col">#</th>
+                                        <th scope="col">#</th>
                                         <th scope="col">Reminders Title</th>
                                         <th scope="col">Due Date</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                               
+                                <tbody>                              
                                 @foreach($reminders as $index => $val)
-                        <tr>
-                            <td>{{++$index}}</td>
-                            <td>{{$val->title}}</td>
-                            <td>1</td>
-                            
-                                
-                                        </tr>
-                                        @endforeach
-                                
+                                    <tr>
+                                        <td>{{++$index}}</td>
+                                        <td>{{$val->title}}</td>
+                                        <td>{{$val->created_at}}</td>
+                                    </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
