edaaf8df87bd7e4e6bca70640bde3d6e286eb2ac
Robin Ortizuela <robinortizuela456@gmail.com>
2023-02-23 15:43:48 +0800

    update algo 30 %

--- app/Http/Controllers/PostGuzzleController.php
+++ app/Http/Controllers/PostGuzzleController.php
@@ -38,36 +38,52 @@ class PostGuzzleController extends Controller
                     ],
                 ]);
             }*/
-            
-            
-                $date = date_create($countdown->dateend, timezone_open('Asia/Manila'));
-                
-                $dateDisplay = date_format($date, "F d, Y H:i:s"); 
-                $dateDisplay1 = date_format($date, "F d, Y "); 
-                $setDate = strtotime($dateDisplay);
-                $remaining = $setDate - time();
-                $days_remaining = floor($remaining / 86400);
-                $hours_remaining = floor(($remaining % 86400) / 3600);
-                
+            if($countdown->type2 == "reminders"){
 
-                return Http::post($countdown->webhook, [
-                    'content' => $countdown->content,
-                    'embeds' => [
-                        [
-                            'title' => $countdown->title,
-                            'description' => $countdown->description . " \n" . $dateDisplay1 . " \n" . $days_remaining . " days and ". $hours_remaining . " hours left",
-                            'color' => '7506394',
-                        ]
-                    ],
-                ]);
+                $bookings = Reminders::where('dateend', '<=', Carbon::now()->add(20, 'minute')->toDateTimeString())
+                    ->where('dateend', '>', Carbon::now()->toDateTimeString())
+                    ->where('notified', 0)
+                    ->get();
+                    foreach($bookings as $booking){
+
+                        //if($booking->dateend >= Carbon::now()->toDateTimeString())
+                        //if($booking->dateend >= Carbon::now()->toDateTimeString()){
+                            $date = date_create($booking->dateend, timezone_open('Asia/Manila'));
+                        
+                            $dateDisplay = date_format($date, "F d, Y H:i:s"); 
+                            $dateDisplay1 = date_format($date, "F d, Y "); 
+                            $setDate = strtotime($dateDisplay);
+                            $remaining = $setDate - time();
+                            $days_remaining = floor($remaining/60/60/24);
+                            $hours_remaining = floor(($remaining-($days_remaining*60*60*24))/60/60);
+                            $minutes_remaining = floor(($remaining-($days_remaining*60*60*24)-($hours_remaining*60*60))/60);
+                            $seconds_remaining = floor(($remaining-($days_remaining*60*60*24)-($hours_remaining*60*60))-($minutes_remaining*60));
+                            
             
+                            return Http::post($booking->webhook, [
+                                'content' => $booking->content,
+                                'embeds' => [
+                                    [
+                                        'title' => $booking->title,
+                                        'description' => $booking->description . " \n" . $dateDisplay1 . " \n" . $days_remaining . " days ". $hours_remaining . " hours and ". $minutes_remaining . " minutes and ". $seconds_remaining . " seconds left ",
+                                        'color' => '7506394',
+                                    ]
+                                ],
+                            ]);
+                        
+            
+                            $booking->notified = 1;
+                            $booking->save();
+                        //}else{
+
+                        //}
+                    }
+                }
         }
-
-       
-
-        //return view('remind-bot.index')->with('countdowns', $countdowns);
+           
+      //return view('remind-bot.index')->with('countdowns', $countdowns);
     }
-
+        
     public function notifyCountdown(){
 
         $countdowns = Countdowns::All();
