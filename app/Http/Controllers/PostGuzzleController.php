7262e6e3fbc12ae763ad979f1ea4ca12c03dbfe6
Robin Ortizuela <robinortizuela456@gmail.com>
2023-02-23 17:20:22 +0800

    update no-img.png

--- app/Http/Controllers/PostGuzzleController.php
+++ app/Http/Controllers/PostGuzzleController.php
@@ -79,6 +79,48 @@ class PostGuzzleController extends Controller
                         //}
                     }
                 }
+                else{
+                    
+                    $bookings = Reminders::where('dateend', '<=', Carbon::now()->add(20, 'minute')->toDateTimeString())
+                    ->where('dateend', '>', Carbon::now()->toDateTimeString())
+                    ->where('notified', 0)
+                    ->get();
+                    foreach($bookings as $booking){
+
+                        //if($booking->dateend >= Carbon::now()->toDateTimeString())
+                        //if($booking->dateend >= Carbon::now()->toDateTimeString()){
+                            $date = date_create($booking->dateend, timezone_open('Asia/Manila'));
+                        
+                            $dateDisplay = date_format($date, "F d, Y H:i:s"); 
+                            $dateDisplay1 = date_format($date, "F d, Y "); 
+                            $setDate = strtotime($dateDisplay);
+                            $remaining = $setDate - time();
+                            $days_remaining = floor($remaining/60/60/24);
+                            $hours_remaining = floor(($remaining-($days_remaining*60*60*24))/60/60);
+                            $minutes_remaining = floor(($remaining-($days_remaining*60*60*24)-($hours_remaining*60*60))/60);
+                            $seconds_remaining = floor(($remaining-($days_remaining*60*60*24)-($hours_remaining*60*60))-($minutes_remaining*60));
+                            
+            
+                            return Http::post($booking->webhook, [
+                                'content' => $booking->content,
+                                'embeds' => [
+                                    [
+                                        'title' => $booking->title,
+                                        'description' => $booking->description . " \n" . $dateDisplay1 . " \n" . $days_remaining . " days ". $hours_remaining . " hours and ". $minutes_remaining . " minutes and ". $seconds_remaining . " seconds left ",
+                                        'color' => '7506394',
+                                    ]
+                                ],
+                            ]);
+                        
+            
+                            $booking->notified = 1;
+                            $booking->save();
+                        //}else{
+
+                        //}
+                    }
+
+                }
         }
            
       //return view('remind-bot.index')->with('countdowns', $countdowns);
