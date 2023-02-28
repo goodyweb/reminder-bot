20a4f0365be54586f7ab3668f72b7cb3373f414b
Robin Ortizuela <robinortizuela456@gmail.com>
2023-02-28 15:36:19 +0800

    dark theme

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FixedDate;
+use App\Models\UnfixedDate;
 use Illuminate\Http\Request;
 
 class ReminderController extends Controller
 {
-    //
+    public function fixDate(){
+        $fixeddates = FixedDate::All();
+        foreach($fixeddates as $fixeddate)
+        {
+            if($fixeddate->frequency == "Annually")
+            {
+
+            }elseif($fixeddate->frequency == "Quarterly")
+            {
+
+            }elseif($fixeddate->frequency == "Monthly")
+            {
+                
+            }
+        }
+    }
 }
