e8bbcdf92e905536d26a48668fbf434f7750eef2
Robin Ortizuela <robinortizuela456@gmail.com>
2023-03-06 10:26:33 +0800

    adding a ReminderController sa iyang functions

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -5,23 +5,123 @@ namespace App\Http\Controllers;
 use App\Models\FixedDate;
 use App\Models\UnfixedDate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
 
 class ReminderController extends Controller
 {
-    public function fixDate(){
-        $fixeddates = FixedDate::All();
-        foreach($fixeddates as $fixeddate)
+    private function updateMonthFixDate($id)
+    {
+
+       // $addmonth = 0;
+        $fixeddate = FixedDate::find($id);
+        
+        if($fixeddate->frequency == "Annually" && $fixeddate->endMonth == Carbon::now()->month && $fixeddate->startDay == Carbon::now()->day)
         {
-            if($fixeddate->frequency == "Annually")
-            {
+            $addmonth = $fixeddate->endMonth;
+            $fixeddate->startMonth = $addmonth;
+            $fixeddate->endMonth = $addmonth;
+            $fixeddate->save();
+            
 
-            }elseif($fixeddate->frequency == "Quarterly")
+        }elseif($fixeddate->frequency == "Quarterly" && $fixeddate->endMonth == Carbon::now()->month && $fixeddate->endDay == Carbon::now()->day)
             {
+                $addmonth = $fixeddate->endMonth + 3;
+                $fixeddate->startMonth = $addmonth;
+                $fixeddate->endMonth = $addmonth;
+                $fixeddate->save();
+                
 
-            }elseif($fixeddate->frequency == "Monthly")
+            }elseif($fixeddate->frequency == "Monthly" && $fixeddate->endMonth == Carbon::now()->month && $fixeddate->endDay == Carbon::now()->day)
             {
+                $addmonth = $fixeddate->endMonth + 1;
+                $fixeddate->startMonth = $addmonth;
+                $fixeddate->endMonth = $addmonth;
+                $fixeddate->save();
                 
             }
+
+            
+        
+        
+    }
+    public function fixDateNotify(){
+
+        $monthNow = Carbon::now()->month;
+        $dayNow = Carbon::now()->day;
+
+        $fixeddates = FixedDate::where('startMonth', '<=', $monthNow)
+        ->where('startDay', '<=', $dayNow)
+        ->get();
+
+        foreach($fixeddates as $fixeddate){
+
+            if($fixeddate->endMonth <= $monthNow ){
+
+                $days_remaining = $fixeddate->endDay - $dayNow;
+                
+               $fixeddated = FixedDate::find($fixeddate->id);
+        
+                if($fixeddate->frequency == "Annually" && $fixeddate->endMonth == Carbon::now()->month && $fixeddate->startDay == Carbon::now()->day)
+                {
+                    $addmonth = $fixeddated->endMonth +1;
+                    $fixeddated->startMonth = $addmonth;
+                    $fixeddated->endMonth = $addmonth;
+                    $fixeddated->save();
+
+                     Http::post($fixeddated->webhook, [
+                        'content' => "Hello, Good day ". $fixeddated->user->name ,
+                        'embeds' => [
+                            [
+                                'title' => $fixeddated->details,
+                                'description' => $days_remaining . " days left ",
+                                'color' => '7506394',
+                            ]
+                        ],
+                    ]);
+
+                }elseif($fixeddate->frequency == "Quarterly" && $fixeddate->endMonth == Carbon::now()->month && $fixeddate->startDay == Carbon::now()->day)
+                {
+                   $addmonth = $fixeddated->endMonth + 3;
+                    $fixeddated->startMonth = $addmonth;
+                    $fixeddated->endMonth = $addmonth;
+                    $fixeddated->save();
+                    
+                     Http::post($fixeddated->webhook, [
+                        'content' => "Hello, Good day ". $fixeddated->user->name ,
+                        'embeds' => [
+                            [
+                                'title' => $fixeddated->details,
+                                'description' => $days_remaining . " days left ",
+                                'color' => '7506394',
+                            ]
+                        ],
+                    ]);
+    
+                }elseif($fixeddate->frequency == "Monthly" && $fixeddate->endMonth == Carbon::now()->month && $fixeddate->startDay == Carbon::now()->day)
+                {
+                    $addmonth = $fixeddated->endMonth + 1;
+                    $fixeddated->startMonth = $addmonth;
+                    $fixeddated->endMonth = $addmonth;
+                    $fixeddated->save();
+
+                     Http::post($fixeddated->webhook, [
+                        'content' => "Hello, Good day ". $fixeddated->user->name ,
+                        'embeds' => [
+                            [
+                                'title' => $fixeddated->details,
+                                'description' => $days_remaining . " days left ",
+                                'color' => '7506394',
+                            ]
+                        ],
+                    ]);
+                    
+                }
+    
+            }
+           
+            
         }
+        return $fixeddates;
     }
 }
