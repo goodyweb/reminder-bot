1b153488459f9cbde36ce4cb24ccfc1f5e1a0504
gracesalvzz <s.salvame.graceann@cmu.edu.ph>
2023-03-20 16:53:58 +0800

    add controller function named Semi

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -197,6 +197,113 @@ class ReminderController extends Controller
     }
 
     public function fixDateNotifyQuarterly(){
+
+        $monthNow = Carbon::now()->month;
+        $dayNow = Carbon::now()->day;
+
+        $fixeddates = FixedDate::where('startMonth', '<=', $monthNow)
+        ->where('endMonth', '>=', $monthNow)
+        ->where('startDay', '<=', $dayNow)
+        ->where('frequency', 'like', 'SemiAnnually')
+        ->get();
+
+        foreach($fixeddates as $fixeddate){
+
+            if($fixeddate->endMonth >= $monthNow ){
+
+                if($fixeddate->endMonth == $monthNow){
+                   
+                    $days_remaining = $fixeddate->endDay - $dayNow;
+                }else{
+                    if($fixeddate->endMonth == 1 || $fixeddate->endMonth == 3 || $fixeddate->endMonth == 5 || $fixeddate->endMonth == 7 || $fixeddate->endMonth == 8 || $fixeddate->endMonth == 10 || $fixeddate->endMonth == 12){
+                        $days_remaining = ($fixeddate->endDay + 31) - $dayNow;
+                    }elseif($fixeddate->endMonth == 4 || $fixeddate->endMonth == 6 || $fixeddate->endMonth == 9 || $fixeddate->endMonth == 11){
+                        $days_remaining = ($fixeddate->endDay + 30) - $dayNow;
+                    }else{
+                        if(date('L', strtotime(Carbon::now())) == 0){
+                            
+                        $days_remaining = ($fixeddate->endDay + 28) - $dayNow;
+                        }else {
+                            
+                        $days_remaining = ($fixeddate->endDay + 29) - $dayNow;
+                        }
+                    }
+                   
+                }
+                
+                     if($fixeddate->endDay == Carbon::now()->day){
+
+                        if($fixeddate->startMonth <= 6 && $fixeddate->endMonth <= 6)
+                        {
+                            $addstartmonth = $fixeddate->startMonth + 6;
+                            $addendmonth = $fixeddate->endMonth + 6;
+                            $fixeddate->startMonth = $addstartmonth;
+                            $fixeddate->endMonth = $addendmonth;
+                            $fixeddate->year = Carbon::now()->year;
+                            
+                        }elseif($fixeddate->startMonth >= 7 && $fixeddate->endMonth >= 7)
+                        {
+
+                            $addstartmonth = ($fixeddate->startMonth + 6) - 12;
+                            $addendmonth = ($fixeddate->endMonth + 6) - 12;
+                            $fixeddate->startMonth = $addstartmonth;
+                            $fixeddate->endMonth = $addendmonth;
+                            $fixeddate->year = Carbon::now()->year +1;
+
+                            
+
+                        }elseif($fixeddate->startMonth <= 6 && $fixeddate->endMonth >= 7)
+                        {
+
+                            $addstartmonth = $fixeddate->startMonth + 6;
+                            $addendmonth = ($fixeddate->endMonth + 6) - 12;
+                            $fixeddate->startMonth = $addstartmonth;
+                            $fixeddate->endMonth = $addendmonth;
+                            $fixeddate->year = Carbon::now()->year +1;
+                            
+
+                        }elseif($fixeddate->startMonth >= 7 && $fixeddate->endMonth <= 6)
+                        {
+
+                            $addstartmonth = ($fixeddate->startMonth + 6) - 12;
+                            $addendmonth = $fixeddate->endMonth + 6;
+                            $fixeddate->startMonth = $addstartmonth;
+                            $fixeddate->endMonth = $addendmonth;
+                            $fixeddate->year = Carbon::now()->year;
+                            
+                        }
+                       
+                        $fixeddate->save();
+
+                        Http::post($fixeddate->webhook, [
+                            'content' => "Hello, Good day ". $fixeddate->user->name ,
+                            'embeds' => [
+                                [
+                                    'title' => $fixeddate->details,
+                                    'description' => "This is your scheduled day of Quarterly " .$fixeddate->details,
+                                    'color' => '7506394',
+                                ]
+                            ],
+                        ]);
+                     }elseif($fixeddate->endDay > Carbon::now()->day ){
+                        Http::post($fixeddate->webhook, [
+                            'content' => "Hello, Good day ". $fixeddate->user->name ,
+                            'embeds' => [
+                                [
+                                    'title' => $fixeddate->details,
+                                    'description' => $days_remaining . " days left ",
+                                    'color' => '7506394',
+                                ]
+                            ],
+                        ]);
+                     }
+
+           
+                }   
+        }
+    }
+
+    public function fixDateNotifyQuarterly(){
         $yearNow = Carbon::now()->year;
         $monthNow = Carbon::now()->month;
         $dayNow = Carbon::now()->day;
