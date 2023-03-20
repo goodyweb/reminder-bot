5786e3754f90e55ce575daca34917dc0b1dd8a02
gracesalvzz <s.salvame.graceann@cmu.edu.ph>
2023-03-20 17:43:00 +0800

    modified

--- app/Console/Commands/RemindAnnuallyCommand.php
+++ app/Console/Commands/RemindAnnuallyCommand.php
@@ -4,33 +4,27 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-<<<<<<<< HEAD:app/Console/Commands/RemindAnnuallyCommand.php
+
 class RemindAnnuallyCommand extends Command
-========
-class RemindSemiAnnuallyCommand extends Command
->>>>>>>> 4c9b5ed5ae499732cea77abe7f0c0f106939be60:app/Console/Commands/RemindSemiAnnuallyCommand.php
+
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-<<<<<<<< HEAD:app/Console/Commands/RemindAnnuallyCommand.php
+
     protected $signature = 'remind:daily1';
-========
-    protected $signature = 'remind:daily4';
->>>>>>>> 4c9b5ed5ae499732cea77abe7f0c0f106939be60:app/Console/Commands/RemindSemiAnnuallyCommand.php
+
 
     /**
      * The console command description.
      *
      * @var string
      */
-<<<<<<<< HEAD:app/Console/Commands/RemindAnnuallyCommand.php
+
     protected $description = 'Remind me everyday with annually';
-========
-    protected $description = 'Remind me everyday';
->>>>>>>> 4c9b5ed5ae499732cea77abe7f0c0f106939be60:app/Console/Commands/RemindSemiAnnuallyCommand.php
+
 
     /**
      * Execute the console command.
@@ -39,10 +33,7 @@ class RemindSemiAnnuallyCommand extends Command
      */
     public function handle()
     {
-<<<<<<<< HEAD:app/Console/Commands/RemindAnnuallyCommand.php
         return app()->call('App\Http\Controllers\ReminderController@fixDateNotifyAnnually');
-========
-        return app()->call('App\Http\Controllers\ReminderController@fixDateNotifySemiAnnually');
->>>>>>>> 4c9b5ed5ae499732cea77abe7f0c0f106939be60:app/Console/Commands/RemindSemiAnnuallyCommand.php
+
     }
 }
