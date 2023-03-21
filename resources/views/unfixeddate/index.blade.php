7be2cd905be4277a0a5024664aba81b1d3a8bd44
Robin Ortizuela <robinortizuela456@gmail.com>
2023-03-21 18:49:10 +0800

    rebase main

--- resources/views/unfixeddate/index.blade.php
+++ resources/views/unfixeddate/index.blade.php
@@ -68,7 +68,7 @@ body, html {
                         <hr>
                         <div class="button-group row">
                           <div class="col-8">
-                            <a href="{{route('reminder_view.show', $val->id)}}" class="btn btn-info btn-sm"><i data-feather="eye"></i>View</a>            
+                            <a href="{{route('unfixeddate.show', $val->id)}}" class="btn btn-info btn-sm"><i data-feather="eye"></i>View</a>            
                             <a href="{{route('unfixeddate.edit', $val->id)}}" class="btn btn-warning btn-sm"><i data-feather="link"></i>Edit</a>
                           </div>
                             <div class="col-4 text-right">
