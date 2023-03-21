6c23272bccfdc9c7410594c82ae63f8cc0ac17f6
Robin Ortizuela <robinortizuela456@gmail.com>
2023-03-21 18:16:25 +0800

    rebase

--- app/Http/Controllers/UnfixedDateController.php
+++ app/Http/Controllers/UnfixedDateController.php
@@ -96,11 +96,6 @@ class UnfixedDateController extends Controller
             'frequency' => 'required'
         ]);
         $input = $request->all();
-<<<<<<< HEAD
-=======
-        
-
->>>>>>> d546735 (rebase main)
         $unfixeddate->update($input);
         return redirect()->route('unfixeddate.index')
             ->with('success','Reminder updated successfully.');
