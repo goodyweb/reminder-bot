07f7d0a97a0068b396e3130d0a64d85a8384dea1
reymarkjan01 <s.gandalla.reymarkjan@cmu.edu.ph>
2023-03-21 18:13:34 +0800

    new dashboad

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="row mt-5">
             <div class="col-xl-8 mb-5 mb-xl-0">
                 <div class="card shadow">
-                        <div class="bg-light card-header border-0">
+                        <div class="bg-info card-header border-0">
                             <div class="row align-items-center">
                                 <div class="col">
                                     <h3 class="mb-0">All Reminders</h3>
@@ -21,45 +21,40 @@
                             <table class="table table-sm table-hover mb-0 align-items-center table-flush">
                                 <thead class="thead-light">
                                     <tr>
-                                    <th class="pt-3"></th>
-                                    <th class="pt-3">#</th>
-                                    <th class="pt-3">Reminder Title</th>
-                                    <th class="pt-3">Content Detail</th>
-                                    <th class="pt-3">Description</th>
                                     <th></th>
+                                    <th class="pt-4">#</th>
+                                    <th class="pt-4">Reminders</th>
+                                    <th class="pt-4">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-
-                                @foreach($reminders as $index => $val)
-                                <tr>
-                                    <td></td>
-                                    <td>{{++$index}}</td>
-                                    <td>{{$val->title}}</td>
-                                    <td>{{$val->content}}</td>
-                                    <td>{{$val->description}}</td>
-                                    <td> <a href="{{route('reminders.show', $val->id)}}" class="btn btn-outline-primary btn-sm">View Details </a></td>
-                                </tr>
-                                @endforeach
+                                    <tr>
+                                        <td></td>
+                                        <td>1</td>
+                                        <td>Fix Reminder Dates</td>
+                                        <td> <a href="{{route('fixeddates.index')}}" class="btn btn-outline-primary btn-sm"><i data-feather="eye"></i> View more details </a></td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>2</td>
+                                        <td>Unfix Reminder Dates</td>
+                                        <td> <a href="{{route('unfixeddate.index')}}" class="btn btn-outline-primary btn-sm"><i data-feather="eye"></i> View more details </a></td>
+                                    </tr>
+                                    <tr><td></td></tr>
                                 </tbody>
                             </table>
-                        </div>
-                        <div class="row mt-3 mb-5">
-                            <div class="col text-center">
-                                <a href="{{route('reminders.create')}}" class="btn btn-info btn-icon-text mb-2 mb-md-0">Create Reminders</a>
-                            </div>
                         </div>
                 </div>
             </div>
             <div class="col-xl-4 mb-5 mb-xl-0">
                 <div class="card shadow">
-                        <div class="bg-light card-header border-0">
+                        <div class="bg-info card-header border-0">
                             <div class="row align-items-center">
                                 <div class="col">
                                     <h3 class="mb-0">Events</h3>
                                 </div>
-                                <div class="col align-items-right">
-                                    <a href="{{route('reminders.index')}}" class="btn btn-sm btn-warning align-items-right">See all</a>
+                                <div class="col-12 col-md-3">
+                                    <a href="{{route('fixeddates.index')}}" class="btn btn-sm btn-warning align-items-right">See all</a>
                                 </div>
                             </div>
                         </div>
@@ -68,15 +63,15 @@
                                 <thead class="thead-light">
                                     <tr>
                                         <th scope="col">#</th>
-                                        <th scope="col">Reminders Title</th>
+                                        <th scope="col">fixeddate Title</th>
                                         <th scope="col">Due Date</th>
                                     </tr>
                                 </thead>
                                 <tbody>                              
-                                @foreach($reminders as $index => $val)
+                                @foreach($unfixeddate as $index => $val)
                                     <tr>
                                         <td>{{++$index}}</td>
-                                        <td>{{$val->title}}</td>
+                                        <td>{{$val->details}}</td>
                                         <td>{{$val->created_at}}</td>
                                     </tr>
                                 @endforeach
--- resources/views/layouts/headers/cards.blade.php
+++ resources/views/layouts/headers/cards.blade.php
@@ -1,18 +1,18 @@
-<div class="header bg-gradient-purple pb-7 pt-5 pt-md-8"> 
     <div class="container-fluid">
         <div class="header-body">
             <!-- Card stats -->
-                <div class="col-xl-4 col-lg-6">
-                    <div class="bg-light card card-stats mb-4 mb-xl-0">
+            <div class="row">
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card card-stats mb-4 mb-xl-0">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col">
-                                    <h5 class="card-title text-uppercase text-muted mb-0">Users</h5>
-                                    <span class="h2 font-weight-bold mb-0">1</span>
+                                <div class="col"> 
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Fixed Date</h5>
+                                    <span class="h2 font-weight-bold mb-0">12</span>
                                 </div>
                                 <div class="col-auto">
-                                    <div class="icon icon-shape bg-info text-white rounded-circle shadow">
-                                        <i class="ni ni-single-02"></i>
+                                    <div class="icon icon-shape bg-primary text-white rounded-circle shadow">
+                                        <i class="link-icon" data-feather="clock"></i>
                                     </div>
                                 </div>
                             </div>
@@ -20,7 +20,68 @@
                                 <span class="text-nowrap">Overall</span>
                             </p>
                         </div>
+                    </div>
                 </div>
-        </div>          
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Unfixed Date</h5>
+                                    <span class="h2 font-weight-bold mb-0">1</span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                        <i class="link-icon" data-feather="clock"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mt-3 mb-0 text-muted text-sm">
+                                <span class="text-nowrap">Overall</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Users</h5>
+                                    <span class="h2 font-weight-bold mb-0">1</span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                                        <i class="link-icon" data-feather="user"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mt-3 mb-0 text-muted text-sm">
+                                <span class="text-nowrap">Overall</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Events</h5>
+                                    <span class="h2 font-weight-bold mb-0">13</span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-info text-white rounded-circle shadow">
+                                        <i class="link-icon" data-feather="book"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mt-3 mb-0 text-muted text-sm">
+                                <span class="text-nowrap">Overall</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>          
     </div>
-</div>
+
