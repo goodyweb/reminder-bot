a079b13b3b33599f4dc81fec09e1c4ec05b9c216
reymarkjan01 <s.gandalla.reymarkjan@cmu.edu.ph>
2023-03-23 10:18:08 +0800

    Dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,12 +8,76 @@
                         <div class="bg-info card-header border-0">
                             <div class="row align-items-center">
                                 <div class="col">
+                                    <h3 class="mb-0">Unfix Date Events</h3>
+                                </div>
+                                <div class="col-12 col-md-2">
+                                    <a href="{{route('unfixeddate.index')}}" class="btn btn-sm btn-warning align-items-right">Manage all</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-sm table-hover mb-0 align-items-center table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th></th>
+                                        <th scope="pt-4">#</th>
+                                        <th scope="pt-4">Unfixed Date Title</th>
+                                        <th scope="pt-4">Due Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>                              
+                                @foreach($unfixeddate as $index => $val)
+                                    <tr>
+                                        <td></td>
+                                        <td>{{++$index}}</td>
+                                        <td>{{$val->details}}</td>
+                                        <td>{{$val->created_at}}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div><div class="bg-info card-header border-0">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <h3 class="mb-0">Fix Date Events</h3>
+                                </div>
+                                <div class="col-12 col-md-2">
+                                    <a href="{{route('fixeddates.index')}}" class="btn btn-sm btn-warning align-items-right">Manage all</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-sm table-hover mb-0 align-items-center table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th></th>
+                                        <th scope="pt-4">#</th>
+                                        <th scope="pt-4">fixeddate Title</th>
+                                        <th scope="pt-4">Due Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>                              
+                                @foreach($fixeddate as $index => $val)
+                                    <tr>
+                                        <td></td>
+                                        <td>{{++$index}}</td>
+                                        <td>{{$val->details}}</td>
+                                        <td>{{$val->endMonth}}/{{$val->endDay}}/{{$val->year}}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>  
+                </div>
+            </div>
+            <div class="col-xl-4 mb-5 mb-xl-0">
+                <div class="card shadow">
+                        <div class="bg-info card-header border-0">
+                            <div class="row align-items-center">
+                                <div class="col">
                                     <h3 class="mb-0">All Reminders</h3>
                                 </div>
-                                
-                                <div class="col text-right">
-                                    
-                                </div>
+                              
                                
                             </div>
                         </div>
@@ -32,13 +96,13 @@
                                         <td></td>
                                         <td>1</td>
                                         <td>Fix Reminder Dates</td>
-                                        <td> <a href="{{route('fixeddates.index')}}" class="btn btn-outline-primary btn-sm"><i data-feather="eye"></i> View more details </a></td>
+                                        <td> <a href="{{route('fixeddates.index')}}" class="btn btn-outline-primary btn-sm"><i data-feather="eye"></i> View all details </a></td>
                                     </tr>
                                     <tr>
                                         <td></td>
                                         <td>2</td>
                                         <td>Unfix Reminder Dates</td>
-                                        <td> <a href="{{route('unfixeddate.index')}}" class="btn btn-outline-primary btn-sm"><i data-feather="eye"></i> View more details </a></td>
+                                        <td> <a href="{{route('unfixeddate.index')}}" class="btn btn-outline-primary btn-sm"><i data-feather="eye"></i> View all details </a></td>
                                     </tr>
                                     <tr><td></td></tr>
                                 </tbody>
@@ -46,46 +110,7 @@
                         </div>
                 </div>
             </div>
-            <div class="col-xl-4 mb-5 mb-xl-0">
-                <div class="card shadow">
-                        <div class="bg-info card-header border-0">
-                            <div class="row align-items-center">
-                                <div class="col">
-                                    <h3 class="mb-0">Events</h3>
-                                </div>
-                                <div class="col-12 col-md-3">
-                                    <a href="{{route('fixeddates.index')}}" class="btn btn-sm btn-warning align-items-right">See all</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="table-responsive">
-                            <table class="table table-sm table-hover mb-0 align-items-center table-flush">
-                                <thead class="thead-light">
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">fixeddate Title</th>
-                                        <th scope="col">Due Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody>                              
-                                @foreach($unfixeddate as $index => $val)
-                                    <tr>
-                                        <td>{{++$index}}</td>
-                                        <td>{{$val->details}}</td>
-                                        <td>{{$val->created_at}}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                   
-                        
-                    
-                </div>
-            </div>
-        </div>
-
-        
+        </div>   
     </div>
 
 
